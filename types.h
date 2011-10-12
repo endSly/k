ef82e23c42e1c7e3ef47cc09eab0ae6952e8efcd
Jon Ander Peñalba <jonan88@gmail.com>
2011-10-12 14:38:24 +0200

    Remove tabs from types.h

--- types.h
+++ types.h
@@ -20,14 +20,14 @@ typedef dword_t*            ptr_t;
 /*
  *  Fixed Size Types
  */
-typedef	unsigned long long  uint64_t;
-typedef	unsigned long       uint32_t;
-typedef	unsigned short      uint16_t;
-typedef	unsigned char       uint8_t;
-typedef	long long           int64_t;
-typedef	long                int32_t;
-typedef	short               int16_t;
-typedef	char                int8_t;
+typedef unsigned long long  uint64_t;
+typedef unsigned long       uint32_t;
+typedef unsigned short      uint16_t;
+typedef unsigned char       uint8_t;
+typedef long long           int64_t;
+typedef long                int32_t;
+typedef short               int16_t;
+typedef char                int8_t;
 
 #ifndef NULL
 #define NULL ((void*)0)
