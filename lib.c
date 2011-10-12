5e913548de1ca96e82e0c662e539396478031f05
Endika Gutiérrez Salas <me@endika.net>
2011-10-12 21:53:47 +0200

    memcpy added.

--- lib.c
+++ lib.c
@@ -13,10 +13,23 @@ int puts(const char* str)
 
 void* memset(void* ptr, int value, size_t num)
 {
-    uint8_t d = ptr;
+    uint8_t* d = ptr;
     while (num--) {
         *d = value;
         d++;
     }
     return ptr;
+}
+
+void* memcpy(void* destination, const void* source, size_t num)
+{
+    uint8_t* d = destination;
+    const uint8_t* s = source;
+    while (num--) {
+        *d = *s;
+        d++;
+        s++;
+    }
+    return destination;
+    
 }
--- lib.h
+++ lib.h
@@ -8,6 +8,7 @@
 int puts(const char* str);
 
 void* memset(void* ptr, int value, size_t num);
+void* memcpy(void* destination, const void* source, size_t num);
 
 
 #endif // __LIB_H__
