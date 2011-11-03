a7fdbd361a18e4890552efeaba519a39299befe8
Endika Gutiérrez Salas <endika.gutierrez@ekaidepd.com>
2011-11-03 10:16:13 +0100

    malloc function implemented.

--- kernel.c
+++ kernel.c
@@ -3,6 +3,7 @@
 #include "arch/screen.h"
 #include "lib/kprintf.h"
 #include "lib/lib.h"
+#include "lib/memory.h"
 
 #define KERNEL_VERSION "0.0.1"
 
@@ -44,6 +45,18 @@ void kernel_main(void* mbd, unsigned int magic)
     interrupt_handlers[32] = &rtc_tick;
 
     kprintf("Ok!\n");
+    
+    // malloc test
+    int* a = kmalloc(10);
+    int* b = kmalloc(20);
+    int* c = kmalloc(30);
+    kprintf("a: 0x%X, b:0x%X, c:0x%X\n", a, b, c);
+    
+    kfree(b);
+    int* b1 = kmalloc(5);
+    int* b2 = kmalloc(5);
+    int* d = kmalloc(50);
+    kprintf("a: 0x%X, b1:0x%X, b2:0x%X, c:0x%X, d:0x%X\n", a, b1, b2, c, d);
 
     int *ptr = (int*)0xA0000000;
     int do_page_fault = *ptr;
--- lib/memory.c
+++ lib/memory.c
@@ -1,7 +1,7 @@
 
 #include "memory.h"
 
-#include "lib.h"
+#include "lib/lib.h"
 #include "arch/paging.h"
 
 /*  Simple alloc algorithm
@@ -13,17 +13,80 @@
  * - After malloc
  *  |mem_block|____data____|mem_block|__data__|mem_block|__free__|
  *
+ * Additionally memory holes are ordered by it's size
+ *
  */
- 
 
-typedef struct {
+typedef struct mem_block {
     size_t size;
-    bool used;
+    struct mem_block* next; // Ordered list of free blocks (size desc)
+    unsigned used  : 1;     // Is a Hole?
+    unsigned magic : 31;    // Magic for error checking
 } mem_block;
+
+#define MAGIC (0x7AAA5555)
+
+// Ordered list of free blocks (size desc)
+static mem_block* free_blocks_list = NULL;
 
 void* kmalloc(size_t size)
 {
+    if (!free_blocks_list || free_blocks_list->size < size) {
+        // We have no hole for alloc this space. Alloc enough pages for size and it's block definition
+        size_t pages_count = 1 + (size + sizeof(mem_block) - 1) / PAGE_SIZE;
+        void* alloc_space = arch_alloc_pages(pages_count, true, true);
+        
+        mem_block* new_block = (mem_block*) alloc_space;
+        
+        new_block->size = pages_count * PAGE_SIZE - sizeof(mem_block);
+        new_block->next = free_blocks_list;
+        new_block->used = false; // Not used by now
+        new_block->magic = MAGIC;
+        
+        // new_block is biggest
+        free_blocks_list = new_block;
+    }
     
+    // Find smallest block to fit it
+    mem_block* prev_smallest_block = NULL;
+    mem_block* smallest_block = free_blocks_list;
+    while (smallest_block->size > size 
+           && smallest_block->next 
+           && smallest_block->next->size >= size) {
+        prev_smallest_block = smallest_block;
+        smallest_block = smallest_block->next;
+    }
+    
+    kassert(smallest_block && smallest_block->size >= (size + sizeof(mem_block)));
+    kassert(smallest_block->magic == MAGIC);
+    smallest_block->used = true;
+    
+    if (prev_smallest_block)
+        prev_smallest_block->next = smallest_block->next;
+    else
+        free_blocks_list = smallest_block->next;
+    
+    if (smallest_block->size > (size + sizeof(mem_block))) {
+        // We have space for split it
+        memblock* new_block = ((void*) smallest_block) + size + sizeof(mem_block);
+        new_block->size = smallest_block->size - size - sizeof(mem_block);
+        new_block->used = false;
+        new_block->magic = MAGIC;
+        
+        smallest_block->size = size;
+        
+        // Insert new block
+        memblock* prev_block = NULL;
+        memblock* block = free_blocks_list;
+        while (block && block->size > new_block->size) {
+            prev_block = block;
+            block = block->next;
+        }
+        prev_block->next = new_block;
+        new_block->next = block;
+    }
+    
+    return ((void*) smallest_block) + sizeof(mem_block);
 }
 
 void* kcalloc(size_t num, size_t size)
