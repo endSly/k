b9c9c1d832ca0cd1dc2e7b6aa1f2f5423fa1dd96
Jon Ander Peñalba <jonan88@gmail.com>
2011-12-05 14:34:14 +0100

    Fix unsigned int and '%' in kprintf

--- lib/kprintf.c
+++ lib/kprintf.c
@@ -9,7 +9,7 @@ static void kprint_int(int32_t i, int base, bool uppercase, uint8_t color)
 {
     uint32_t divisor = 1;
 
-    if (i < 0 && base == 10) {
+    if (i < 0 && base == 10 && !uppercase) {
         arch_putc_color('-', color);
         i = -i;
     }
@@ -33,11 +33,6 @@ static void kprint_int(int32_t i, int base, bool uppercase, uint8_t color)
     } while (divisor >= 1);
 }
 
-static void kprint_float(double f)
-{
-
-}
-
 void vkprintf(const char* format, va_list vl)
 {
     uint8_t color = SCREEN_COLOR(BLACK, L_GREY);
@@ -47,7 +42,7 @@ void vkprintf(const char* format, va_list vl)
             s++;
 
             switch (*s) {
-                case 'c': { // Char
+                case 'c': { // Character
                     char c = va_arg(vl, char);
                     arch_putc_color(c, color);
                     break;
@@ -58,39 +53,38 @@ void vkprintf(const char* format, va_list vl)
                         arch_putc_color(*str, color);
                     break;
                 }
-                case 'd': { // Decimal
+                case 'd': // Signed decimal integer
+                case 'i': {
                     int d = va_arg(vl, int);
                     kprint_int(d, 10, false, color);
                     break;
                 }
-                case 'l': { // Long
-                    // Long types are not supported!
-                    break;
-                }
-                case 'o': { // Octal
-                    int d = va_arg(vl, int);
-                    kprint_int(d, 8, false, color);
-                    break;
-                }
-                case 'x': { // Hex
-                    int d = va_arg(vl, int);
-                    kprint_int(d, 16, false, color);
-                    break;
-                }
-                case 'X': { // Hex uppercase
-                    int d = va_arg(vl, int);
-                    kprint_int(d, 16, true, color);
-                    break;
-                }
-                case 'u': { // Unsigned
+                case 'u': { // Unsigned decimal integer
                     unsigned int d = va_arg(vl, unsigned int);
                     kprint_int(d, 10, true, color);
                     break;
                 }
-                case '$': { // Change color
-                    color = va_arg(vl, char);
+                case 'o': { // Unsigned octal
+                    int d = va_arg(vl, int);
+                    kprint_int(d, 8, false, color);
                     break;
                 }
+                case 'x': { // Unsigned hexadecimal integer
+                    int d = va_arg(vl, int);
+                    kprint_int(d, 16, false, color);
+                    break;
+                }
+                case 'X': { // Unsigned hexadecimal integer (capital letters)
+                    int d = va_arg(vl, int);
+                    kprint_int(d, 16, true, color);
+                    break;
+                }
+                case '$': // Change color
+                    color = va_arg(vl, char);
+                    break;
+                case '%': // %
+                    arch_putc_color('%', color);
+                    break;
                 default: // Do nothing
                     break;
 
